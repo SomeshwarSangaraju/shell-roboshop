d287deba233bc66c866ebcb8b2cde9166cfefe07
SSR <someshwar.sangaraju19@gmail.com>
2025-09-30 15:18:44 +0530

    creating instance through shell

--- roboshop.sh
+++ roboshop.sh
@@ -7,7 +7,8 @@ DOMAIN_NAME="someshwar.fun"
 
 for instance in $@
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]' --query 'Instances[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+
 
       # Get Private IP
     if [ $instance != "frontend" ]; then
