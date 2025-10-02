b1b4f6d449b417334a6c11aca04a2117a4930c9f
SSR <someshwar.sangaraju19@gmail.com>
2025-10-02 15:56:36 +0530

    mongodb running with shell script

--- mongodb.sh
+++ mongodb.sh
@@ -8,6 +8,12 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+LOG_FOLDER='/var/log/shell-roboshop'
+SCRIPT_NAME=$(echo $0 | cut -d "." f1)
+LOG_FILES=$LOG_FOLDER/$SCRIPT_NAME.log      #/var/log/shell-roboshop/16-logs.log
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOG_FOLDER
 #root user is created at starting so root id is 0
 if [ $USERID -ne 0 ]; then
     echo "Please run the script with root previlages"
@@ -17,21 +23,40 @@ fi
 #checking the package is success or failure
 VALIDATE(){
     if [ $? -ne 0 ]; then
-        echo -e "ERROR:: Installing is ...... $R FAILURE $N"
+        echo -e "$1 ...... $R FAILURE $N"
         exit 1
     else
-        echo -e "SUCCESS:: Installing is ........ $G SUCCESS $N"
+        echo -e "$1 ........ $G SUCCESS $N"
     fi
 }
     
+cp $SCRIPT_DIR /etc/yum.repo.d/mongo.repo
+VALIDATE "Adding mongo repo"
 
-for package in $@
-do
-    dnf list installed mysql
-    if [ $? -ne 0 ]; then
-        dnf install $package -y
-        VALIDATE $package
-    else
-        echo -e "$package is already exists..... $Y SKIPPING $N"
-    fi
-done
+
+dnf install mongodb-org -y 
+VALIDATE "Installing mongodb"
+
+systemctl enable mongod 
+VALIDATE "Enabling mongodb"
+
+systemctl start mongod 
+VALIDATE "Starting mongodb"
+
+sed -i '/s/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE "Allowing remote connections to mongodb"
+
+systemctl restart mongodb
+VALIDATE "Restarting mongodb"
+
+
+# for package in $@
+# do
+#     dnf list installed mysql
+#     if [ $? -ne 0 ]; then
+#         dnf install $package -y
+#         VALIDATE $package
+#     else
+#         echo -e "$package is already exists..... $Y SKIPPING $N"
+#     fi
+# done
