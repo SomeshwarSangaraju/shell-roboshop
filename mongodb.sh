53e8c5b0c4b6e5af14c743acde356bb501d5a426
SSR <someshwar.sangaraju19@gmail.com>
2025-10-02 16:04:25 +0530

    shell-script

--- mongodb.sh
+++ mongodb.sh
@@ -44,7 +44,7 @@ VALIDATE "Enabling mongodb"
 systemctl start mongod 
 VALIDATE "Starting mongodb"
 
-sed -i '/s/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 VALIDATE "Allowing remote connections to mongodb"
 
 systemctl restart mongodb
