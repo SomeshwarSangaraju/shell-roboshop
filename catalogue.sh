71895f7dc77fe6d8fc0055595c86b68ebd734e1e
SSR <someshwar.sangaraju19@gmail.com>
2025-10-02 20:52:50 +0530

    Catalogue running through shell-script

---
+++ catalogue.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOG_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." f1 )
+LOG_FILE=$LOG_FOLDER/$SCRIPT_NAME.log
+SCRIPT_DIR=$PWD
+MONGODB_IP=mongodb.someshwar.fun
+
+mkdir -p LOG_FOLDER
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: please run the script with root previlage"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 is ..... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 is ...... $G SUCCESS $N" | tee -a $LOG_FILE
+    fi
+}
+
+dnf module disable nodejs -y
+VALIDATE $? "Disabling nodejs" &>> $LOG_FILE
+
+dnf module enable nodejs:20 -y
+VALIDATE $? "Enabling nodejs" &>> $LOG_FILE
+
+dnf install nodejs -y
+VALIDATE $? "Installing nodejs" &>> $LOG_FILE
+
+id roboshop 
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop | tee -a $LOG_FILE
+    VALIDATE $? "Creating System user"
+else
+    echo -e "System user is already exist....... $Y SKIPPING $N" &>> $LOG_FILE
+fi
+
+mkdir -p /app 
+VALIDATE $? "creating directory" &>> $LOG_FILE
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
+VALIDATE $? "Downloading catalogue code" &>> $LOG_FILE
+
+cd /app 
+VALIDATE $? "Changing directory" &>> $LOG_FILE
+
+unzip /tmp/catalogue.zip
+VALIDATE $? "Unziping catalogue" &>> $LOG_FILE
+
+npm install 
+VALIDATE $? "Installing dependencies" &>> $LOG_FILE
+
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
+VALIDATE $? "Accessing services" &>> $LOG_FILE
+
+systemctl daemon-reload
+VALIDATE $? "Reloading catalogue" &>> $LOG_FILE
+
+systemctl enable catalogue 
+VALIDATE $? "Enabling catalogue" &>> $LOG_FILE
+
+systemctl start catalogue
+VALIDATE $? "Starting catalogue" &>> $LOG_FILE
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Accessing mongo repo" | tee -a $LOG_FILE
+
+dnf install mongodb-mongosh -y
+VALIDATE $? "Installing mongodb" &>> $LOG_FILE
+
+mongosh --host $MONGODB_IP </app/db/master-data.js
+VALIDATE $? "Loading Master data" | tee -a $LOG_FILE
+
+mongosh --host $MONGODB_IP
+VALIDATE $? "Checking connected to mongodb or not" | tee -a $LOG_FILE
+
+show dbs
+VALIDATE $? "showing dbs" | tee -a $LOG_FILE
+
+use catalogue
+VALIDATE $? "Selecting database" | tee -a $LOG_FILE
+
+show collections
+
+db.products.find()
+
+
+
+
+
+
+
+
+
+
